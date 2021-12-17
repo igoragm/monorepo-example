c0139da383f8d1898c274bf75501fa1a0fd3d68c
Igor Vasiljevic <igor.vasiljevic@denkwerk.com>
2021-12-17 07:43:46 +0100

    Moved Characters data setup to Main.tsx

--- apps/got/src/components/characters/Characters.tsx
+++ apps/got/src/components/characters/Characters.tsx
@@ -1,49 +1,12 @@
 import * as React from "react";
-import { connect } from "react-redux";
-import { getCharacters } from "../../store/actions/characters.actions";
 import { Table } from "antd";
 
-const mapDispatchToProps = { getCharacters };
-const mapStateToProps = ({ characters }) => ({ charactersList: characters.characters });
+export type CharactersProps = { charactersList: any; columns: Record<string, unknown> };
 
-const columns = [
-    {
-        title: "Family",
-        dataIndex: "family",
-        sorter: (a, b) => a.family.length - b.family.length
-    },
-    {
-        title: "Name",
-        dataIndex: "fullName",
-        sorter: (a, b) => a.fullName.length - b.fullName.length
-    },
-    {
-        title: "Title",
-        dataIndex: "title",
-        sorter: (a, b) => a.title.length - b.title.length
-    }
-];
-
-class Characters extends React.Component<any, any> {
-    state = {
-        charactersList: [] as any
-    };
-
-    async componentDidMount() {
-        await this.props.getCharacters();
-        const { charactersList } = this.props;
-        const data = charactersList.data.map(character => {
-            return {
-                key: character.id,
-                family: character.family,
-                fullName: character.fullName,
-                title: character.title
-            };
-        });
-        this.setState({ charactersList: data });
-    }
-
+export class Characters extends React.Component<CharactersProps> {
     render() {
+        const { charactersList, columns } = this.props;
+
         return (
             <div>
                 <Table
@@ -58,11 +21,9 @@ class Characters extends React.Component<any, any> {
                         };
                     }}
                     columns={columns}
-                    dataSource={this.state.charactersList}
+                    dataSource={charactersList}
                 />
             </div>
         );
     }
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(Characters);
--- apps/got/src/containers/Main/Main.tsx
+++ apps/got/src/containers/Main/Main.tsx
@@ -1,18 +1,62 @@
 import * as React from "react";
-import Characters from "../../components/characters/Characters";
+import { Characters } from "../../components/characters/Characters";
 import MainLayout from "../../components/layout/MainLayout";
 import CharacterDetail from "../../components/characterDetail/CharacterDetail";
 import styles from "./Main.module.scss";
+import { connect } from "react-redux";
+import { getCharacters } from "../../store/actions/characters.actions";
 
-export default function Main() {
-    return (
-        <MainLayout>
-            <div className={styles.left}>
-                <Characters />
-            </div>
-            <div className={styles.right}>
-                <CharacterDetail />
-            </div>
-        </MainLayout>
-    );
+const mapDispatchToProps = { getCharacters };
+const mapStateToProps = ({ characters }) => ({ charactersList: characters.characters });
+
+const columns = [
+    {
+        title: "Family",
+        dataIndex: "family",
+        sorter: (a, b) => a.family.length - b.family.length
+    },
+    {
+        title: "Name",
+        dataIndex: "fullName",
+        sorter: (a, b) => a.fullName.length - b.fullName.length
+    },
+    {
+        title: "Title",
+        dataIndex: "title",
+        sorter: (a, b) => a.title.length - b.title.length
+    }
+];
+
+class Main extends React.Component<any, any> {
+    state = {
+        charactersList: [] as any
+    };
+
+    async componentDidMount() {
+        await this.props.getCharacters();
+        const { charactersList } = this.props;
+        const data = charactersList.data.map(character => {
+            return {
+                key: character.id,
+                family: character.family,
+                fullName: character.fullName,
+                title: character.title
+            };
+        });
+        this.setState({ charactersList: data });
+    }
+
+    render() {
+        return (
+            <MainLayout>
+                <div className={styles.left}>
+                    <Characters charactersList={this.state.charactersList} columns={columns} />
+                </div>
+                <div className={styles.right}>
+                    <CharacterDetail />
+                </div>
+            </MainLayout>
+        );
+    }
 }
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
